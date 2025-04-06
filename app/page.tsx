45e7b142da99ade3558e8dd2138b4b23f9c17d60
jacobjshelp <jacobjshelp>
2025-04-06 20:24:15 +0400

    Using Header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
+import Header from "./components/Header";
 import HourlyForecast from "./components/HourlyForecast";
 
 export default function Home() {
   return (
     <div className="h-dvh flex flex-col font-mono">
-      <header className="min-h-2/10 flex items-center justify-center">
-        <h1>
-          Better weather
-        </h1>
-      </header>
+      <Header />
       <main className="h-8/10 w-9/10 mx-auto mb-4">
         <HourlyForecast />
       </main>
