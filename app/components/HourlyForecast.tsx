8c57a5805e2f1566d1821f29e6d2e04d8bf093fb
jacobjshelp <jacobjshelp>
2025-04-06 18:59:05 +0400

    css math working (still wip)

--- app/components/HourlyForecast.tsx
+++ app/components/HourlyForecast.tsx
@@ -1,4 +1,4 @@
-import { HOURS_IN_DAY } from "../constants";
+import { HOURS_IN_DAY, TORRENTIAL_RAIN } from "../constants";
 import {
   getMockWeatherData,
   getWeatherData,
@@ -6,14 +6,23 @@ import {
 
 export default async function HourlyForecast() {
   const { hourly } = await getMockWeatherData();
-  const today = hourly.precipitation.slice(0, HOURS_IN_DAY)
+  const today = hourly.precipitation.slice(0, HOURS_IN_DAY);
 
   return (
     <div className="grid grid-cols-4 gap-4 h-full">
       {today.map((value, i) => {
+        const percentageToFill = Number(((value / TORRENTIAL_RAIN) * 100).toFixed(0));
+        const bar = Math.ceil(percentageToFill / 10) * 10
+        const foo = `bg-gradient-to-t from-blue-500 to-${bar || 10}%`
+
         return (
-          <div key={i} className="bg-blue-500">{value}</div>
-        )
+          <div
+            key={i}
+            className={foo}
+          >
+            {value}
+          </div>
+        );
       })}
     </div>
   );
