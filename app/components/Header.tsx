5c22da152defda7203a6415e60c9d7a525086eb8
jacobjshelp <jacobjshelp>
2025-04-06 19:58:05 +0400

    added Header

---
+++ app/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="min-h-2/10 flex items-center justify-center">
+      <h1>
+        Better weather
+      </h1>
+    </header>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,13 @@ import HourlyForecast from "./components/HourlyForecast";
 
 export default function Home() {
   return (
-    <div className="h-dvh flex">
-      <main className="h-9/10 w-9/10 mx-auto my-auto">
+    <div className="h-dvh flex flex-col font-mono">
+      <header className="min-h-2/10 flex items-center justify-center">
+        <h1>
+          Better weather
+        </h1>
+      </header>
+      <main className="h-8/10 w-9/10 mx-auto mb-4">
         <HourlyForecast />
       </main>
     </div>
