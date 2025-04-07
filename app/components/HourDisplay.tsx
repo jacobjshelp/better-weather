f5cf8591d8dc2c3054f7788d968df5078b18e278
jacobjshelp <jacobjshelp>
2025-04-07 19:12:34 +0400

    refactored into HourDisplay

---
+++ app/components/HourDisplay.tsx
@@ -0,0 +1,29 @@
+type HourDisplayProps = {
+  precipitation: string;
+  probability: string;
+  gradient: string;
+  time: string;
+};
+
+export default function HourDisplay({
+  precipitation,
+  probability,
+  gradient,
+  time,
+}: HourDisplayProps) {
+  return (
+    <div
+      className={`p-[2px] border-2 border-solid rounded-md border-cyan-100 grid grid-cols-2 grid-rows-3 text-sm ${gradient}`}
+    >
+      <div className="text-left">{precipitation}</div>
+      <div className="text-right">{probability}</div>
+
+      <div className="flex col-span-2 items-end justify-center font-bold">
+        {time}
+      </div>
+
+      <div className="flex items-end"></div>
+      <div className="flex items-end ml-auto"></div>
+    </div>
+  );
+}
--- app/components/OneDayForecast.tsx
+++ app/components/OneDayForecast.tsx
@@ -1,5 +1,6 @@
 import calculateGradient from "../functions/calculateGradient";
 import { HourlyData, WeatherDataDTO } from "../types";
+import HourDisplay from "./HourDisplay";
 
 type OneDayForecastProps = {
   dataForOneDay: HourlyData[];
@@ -14,22 +15,16 @@ export default function OneDayForecast({ dataForOneDay }: OneDayForecastProps) {
           hour.precipitation !== 0 ? `${hour.precipitation.toFixed()}mm` : "";
         const probability =
           hour.precipitation !== 0 ? `${hour.precipitation_probability}%` : "";
+        const time = hour.time.split("T")[1];
 
         return (
-          <div
+          <HourDisplay
             key={i}
-            className={`p-[2px] border-2 border-solid rounded-md border-cyan-100 grid grid-cols-2 grid-rows-3 text-sm ${gradient}`}
-          >
-            <div className="text-left">{precipitation}</div>
-            <div className="text-right">{probability}</div>
-
-            <div className="flex col-span-2 items-end justify-center font-bold">
-              {hour.time.split("T")[1]}
-            </div>
-
-            <div className="flex items-end"></div>
-            <div className="flex items-end ml-auto"></div>
-          </div>
+            precipitation={precipitation}
+            probability={probability}
+            gradient={gradient}
+            time={time}
+          />
         );
       })}
     </div>
