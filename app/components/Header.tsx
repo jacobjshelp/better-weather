f1dd04da5a2577a4139d26cdff9345f02aa0bced
jacobjshelp <jacobjshelp>
2025-04-07 16:40:49 +0400

    bigger font for header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="min-h-2/10 flex items-center justify-center">
-      <h1>
+      <h1 className="text-2xl">
         Better weather
       </h1>
     </header>
