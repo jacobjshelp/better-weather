928be7744dea73e36807558d13072feb99e3d72e
jacobjshelp <jacobjshelp>
2025-04-07 19:12:34 +0400

    refactored into HourDisplay
    removed unused divs
    
    removed unused parameter
    
    fixed styling
    
    overflow scroll y

--- app/components/DateDisplay.tsx
+++ app/components/DateDisplay.tsx
@@ -1,9 +1,11 @@
 type DateDisplayProps = {
-  date: Date
-}
+  date: Date;
+};
 
 export default function DateDisplay({ date }: DateDisplayProps) {
   return (
-    <h2 className="flex justify-center items-end h-1/10 text-lg pb-2">{date.toLocaleDateString()}</h2>
-  )
+    <h2 className="flex justify-center items-end h-auto text-lg pb-2">
+      {date.toLocaleDateString()}
+    </h2>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,16 @@
 import ForecastCarousel from "./components/ForecastCarousel";
 import Header from "./components/Header";
-import { DAYS_IN_FORECAST } from "./constants";
 import { getMockWeatherData } from "./functions/getWeatherData";
 import splitIntoDays from "./functions/splitIntoDays";
 
 export default async function Home() {
   const weatherData = await getMockWeatherData();
-  const allDays = splitIntoDays(weatherData, DAYS_IN_FORECAST);
+  const allDays = splitIntoDays(weatherData);
 
   return (
     <div className="h-dvh flex flex-col font-mono">
       <Header />
-      <main className="h-8/10 w-9/10 mx-auto mb-4">
+      <main className="h-9/10 w-9/10 mx-auto">
         <ForecastCarousel allDays={allDays} />
       </main>
     </div>
