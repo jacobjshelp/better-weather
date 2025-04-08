03c0bc02c6d62108f413b262da501eacb252f516
jacobjshelp <jacobjshelp>
2025-04-08 16:29:13 +0400

    ran prettier

--- app/functions/calculateGradient.ts
+++ app/functions/calculateGradient.ts
@@ -1,11 +1,25 @@
-import { TORRENTIAL_RAIN } from "../constants";
+import {
+  RAIN_LOWER_LIMIT,
+  RAINY_WEATHER_STYLE,
+  SUNNY_WEATHER_STYLE,
+  TORRENTIAL_RAIN,
+} from "../constants";
+import { clampNumber, roundUpToNearestTen } from "./mathUtils";
 
 export default function calculateGradient(rainAmount: number) {
-  const gradientPercentage = (rainAmount / TORRENTIAL_RAIN) * 100;
-  const roundedToTen = Math.ceil(Number(gradientPercentage) / 10) * 10;
+  const clampedAmount = clampNumber(
+    rainAmount,
+    RAIN_LOWER_LIMIT,
+    TORRENTIAL_RAIN,
+  );
+
+  const gradientPercentage = (clampedAmount / TORRENTIAL_RAIN) * 100;
+  const roundedUp = roundUpToNearestTen(gradientPercentage);
+
   const gradientClass =
-    roundedToTen !== 0
-      ? `bg-gradient-to-t from-blue-800 from-${roundedToTen}% to-${roundedToTen}%`
-      : "bg-yellow-400 text-black";
+    roundedUp !== 0
+      ? `${RAINY_WEATHER_STYLE} from-${roundedUp}% to-${roundedUp}%`
+      : SUNNY_WEATHER_STYLE;
+
   return gradientClass;
 }
